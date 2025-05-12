ff2ba18dd1c0aa628a9eab23794e29b2c796f814
Juan Pablo Sáenz <jpsaenzmo@gmail.com>
2025-05-12 17:56:28 +0200

    Uploaded class exercise

--- esercizi/10-authentication/esercizio/app.py
+++ esercizi/10-authentication/esercizio/app.py
@@ -1,32 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+from flask_login import LoginManager, login_user, login_required, current_user
+from models import User
+
 import studenti_dao, aule_dao
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "Chiave para c'è posto"
 
-lista = [
-    {
-        "id": 0,
-        "nome": "Aula 1",
-        "descrizione": "Descrizione Aula 1",
-        "capienza": 50,
-        "img": "images/aula-1.jpg",
-    },
-    {
-        "id": 1,
-        "nome": "Aula 2",
-        "descrizione": "Descrizione Aula 2",
-        "capienza": 20,
-        "img": "images/aula-2.jpg",
-    },
-    {
-        "id": 2,
-        "nome": "Aula 3",
-        "descrizione": "Descrizione Aula 3",
-        "capienza": 100,
-        "img": "images/aula-3.jpg",
-    },
-]
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route("/")
@@ -35,6 +18,7 @@ def home():
 
 
 @app.route("/aule")
+@login_required
 def aule():
     lista_aule_db = aule_dao.get_aule()
     return render_template("aule.html", p_aule=lista_aule_db)
@@ -51,18 +35,63 @@ def signup():
     return render_template("sign-up.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 @app.route("/subscribe", methods=["POST"])
 def nuovo_utente():
     nome = request.form.get("txt_nome")
     cognome = request.form.get("txt_cognome")
     email = request.form.get("txt_email")
+    password = request.form.get("txt_password")
 
-    studenti_dao.nuovo_utente(nome, cognome, email)
+    studenti_dao.nuovo_utente(nome, cognome, email, password)
 
     return redirect(url_for("home"))
 
 
-@app.route("/prenotare")
+@app.route("/autenticare", methods=["POST"])
+def autenticare_utente():
+
+    utente_form = request.form.to_dict()
+    utente_db = studenti_dao.get_user_by_email(utente_form["txt_email"])
+
+    if not utente_db:
+        print("Credenziali non valide")
+        return redirect(url_for("home"))
+    else:
+        new = User(
+            id=utente_db["id"],
+            nome=utente_db["nome"],
+            cognome=utente_db["cognome"],
+            email=utente_db["email"],
+            password=utente_db["password"],
+        )
+
+        login_user(new)
+
+        return redirect(url_for("home"))
+
+
+@app.route("/prenotazione")
 def prenotazione():
-    lista_id_nome = [{"id": item["id"], "nome": item["nome"]} for item in lista]
-    return render_template("prenotazione.html", p_aule=lista_id_nome)
+    return redirect(url_for("home"))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_user = studenti_dao.get_user_by_id(user_id)
+    if db_user is not None:
+        user = User(
+            id=db_user["id"],
+            nome=db_user["nome"],
+            cognome=db_user["cognome"],
+            email=db_user["email"],
+            password=db_user["password"],
+        )
+    else:
+        user = None
+
+    return user
--- esercizi/10-authentication/esercizio/studenti_dao.py
+++ esercizi/10-authentication/esercizio/studenti_dao.py
@@ -1,13 +1,49 @@
 import sqlite3
 
-def nuovo_utente(p_nome, p_cognome, p_email):
 
-  sql = "INSERT INTO utenti (nome, cognome, email) VALUES (?, ?, ?)"
-  
-  conn = sqlite3.connect('ceposto.db')
-  cursor = conn.cursor()
-  cursor.execute(sql, (p_nome, p_cognome, p_email))
-  
-  conn.commit()
-  cursor.close()
-  conn.close()
+def nuovo_utente(p_nome, p_cognome, p_email, p_password):
+
+    sql = "INSERT INTO studenti (nome, cognome, email, password) VALUES (?, ?, ?, ?)"
+
+    conn = sqlite3.connect("ceposto.db")
+    cursor = conn.cursor()
+    cursor.execute(sql, (p_nome, p_cognome, p_email, p_password))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def get_user_by_id(p_id):
+
+    sql = "SELECT * FROM studenti WHERE id = ?"
+
+    conn = sqlite3.connect("ceposto.db")
+
+    conn.row_factory = sqlite3.Row
+
+    cursor = conn.cursor()
+    cursor.execute(sql, (p_id,))
+    user = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return user
+
+def get_user_by_email(p_email):
+
+    sql = "SELECT * FROM studenti WHERE email = ?"
+
+    conn = sqlite3.connect("ceposto.db")
+
+    conn.row_factory = sqlite3.Row
+
+    cursor = conn.cursor()
+    cursor.execute(sql, (p_email,))
+    user = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return user
